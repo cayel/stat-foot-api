e6b18797918e42b21af556d409b8a0ba6259d551
Christophe Ayel <CAYEL@cegid.fr>
2018-10-11 13:41:01 +0200

    Api ranking

--- index.js
+++ index.js
@@ -18,6 +18,23 @@ app.get('/', function (req, res) {
   res.json(data);
 });
 
+app.get('/ranking', function (req, res) {
+  var season = req.query.season;
+  if (season == 2017) {
+    var data = {
+      "PSG": [1,75,30,6,2,67,34],
+      "Olympique Lyonnais": [2,72,28,6,4,73,49],
+      "OM": [3,69,27,6,5,49,42]
+    };  
+  } else if (season == 2016) {
+    var data = {
+      "PSG": [1,71,30,6,2,67,34],
+      "Monaco": [2,65,28,6,4,73,49],
+      "Metz": [3,61,27,6,5,49,42]
+    };  
+  }
+  res.json(data);
+});
 
 var server = app.listen(3000, function () {
 
