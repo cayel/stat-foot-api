fe2a728bec1b667523560aaaa0f0b989b8bc523d
Christophe Ayel <christophe.ayel@gmail.com>
2018-10-26 23:28:22 +0200

    Handle errors

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ var express = require('express');
 var cors = require('cors');
 var app = express();
 var Ranking = require ('./ranking');
-var swaggerUi = require('swagger-ui-express'),
+var swaggerUi = require('swagger-ui-express');
+const boom = require('boom')
 
 PORT = process.env.PORT || 5000
 
@@ -40,10 +41,20 @@ async function evalLeagueRanking(season) {
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-app.get('/ranking', function (req, res) {
+app.get('/ranking', function (req, res, next) {
   var season = req.query.season;
+  if (!season) return next(boom.badRequest('missing season'));
   evalLeagueRanking(season).then( r => res.json(r));
 });
+
+app.use((err, req, res, next) => {
+  if (err.isServer) {
+    // log the error...
+    // probably you don't want to log unauthorized access
+    // or do you?
+  }
+  return res.status(err.output.statusCode).json(err.output.payload);
+})
 
 var server = app.listen(PORT, function () {
 
